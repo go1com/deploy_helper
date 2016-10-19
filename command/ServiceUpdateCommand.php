f3904e5a6e95fd25378dfaf9fddf2e76e4757516
Andy Truong <thehongtt@gmail.com>
2016-10-19 16:13:16 +0700

    Move the update logic to ServiceUpdateCommand.

---
+++ command/ServiceUpdateCommand.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace go1\deploy_helper\command;
+
+use GuzzleHttp\Client;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ServiceUpdateCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('service:update')
+            ->addArgument('service', InputArgument::REQUIRED)
+            ->addArgument('environment', InputArgument::REQUIRED)
+            ->addOption('delay', null, InputOption::VALUE_OPTIONAL, '', 5)
+            ->addOption('limit', null, InputOption::VALUE_OPTIONAL, '', 20);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $service = $input->getArgument('service');
+        $environment = $input->getArgument('environment');
+        $delay = $input->getOption('delay');
+        $limit = $input->getOption('limit');
+        $url = ('production' === $environment) ? 'https://api.mygo1.com/v3' : "http://api-{$environment}.mygo1.com/v3";
+        $url .= "/{$service}-service/install";
+
+        return $this->update($output, $url, $delay, $limit);
+    }
+
+    private function update(OutputInterface $output, $url, $delay, $limit)
+    {
+        if ($limit > 0) {
+            $res = (new Client)->get($url, ['http_errors' => false]);
+            if (!in_array($res->getStatusCode(), [200, 204, 400])) {
+                $output->writeln("[FAILED] Status code: {$res->getStatusCode()}. Try again in 5 seconds.");
+                sleep($delay);
+
+                return $this->update($output, $url, $delay, $limit - 1);
+            }
+
+            return $output->writeln('[ERROR] Too many fails.');
+        }
+
+        return $output->writeln('[ERROR] Too many fails.');
+    }
+}
--- index.php
+++ index.php
@@ -2,37 +2,14 @@
 
 namespace go1\ci_helper;
 
-use GuzzleHttp\Client;
+use go1\deploy_helper\command\EndpointBuilderCommand;
+use go1\deploy_helper\command\ServiceUpdateCommand;
+use Symfony\Component\Console\Application;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$env = isset($argv[1]) ? $argv[1] : null;
-$service = isset($argv[2]) ? $argv[2] : null;
-if (!$env || !$service) {
-    echo "Usage: php deploy_helper.phar ENV SERVICE. Example: php deploy_helper.phar production rules\n";
-    exit;
-}
+$app = new Application('GO1');
+$app->add(new ServiceUpdateCommand);
+$app->add(new EndpointBuilderCommand);
 
-$url = ('production' === $env) ? 'https://api.mygo1.com/v3' : "http://api-{$env}.mygo1.com/v3";
-$url .= "/{$service}-service/install";
-
-$try = 0;
-
-while (true) {
-    if (20 >= $try++) {
-        $client = new Client;
-        $response = $client->get($url, ['http_errors' => false]);
-        if (!in_array($response->getStatusCode(), [200, 204, 400])) {
-            echo "[FAILED] Status code: {$response->getStatusCode()}. Try again in 5 seconds.\n";
-            sleep(5);
-            continue;
-        }
-
-        echo "[OK] Status code: {$response->getStatusCode()} \n";
-
-        break;
-    }
-
-    echo "[ERROR] Too many fails.\n";
-    break;
-}
+return $app->run();
